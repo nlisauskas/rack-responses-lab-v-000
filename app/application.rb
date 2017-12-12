9c91f1e29fa4ee42aeb287cc3a4bc3b73415eac2
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-12 16:01:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,19 @@
 class Application
   # if before noon, "Good Morning!"
   # if afternoon, "Good Afternoon!"
-  
-end
 
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.now(hour) < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    
+    resp.finish
+  end
   
+
+end
